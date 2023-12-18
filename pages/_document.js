d8205ca7e90e569bc421e6c1700bf88a47a8cdc0
Armnando Cesar Martin <armandoc9943@gmail.com>
2023-12-18 08:26:42 -0500

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,11 @@
-import Document, { 
-  Html, 
-  Head, 
-  Main, 
-  NextScript 
-} from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="zxx">
         <Head>
-          <link rel="icon" href="/images/favicon.png" />
+          <link rel="icon" href="/images/black-logo.png" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
             rel="stylesheet"
